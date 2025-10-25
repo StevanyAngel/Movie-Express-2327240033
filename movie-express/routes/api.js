e90962d87edac75f7d4988c3ce3e267ac8eb8914
StevanyAngel <stevanyangel_2327240033@mhs.mdp.ac.id>
2025-10-25 22:32:17 +0700

    update api.js

--- movie-express/routes/api.js
+++ movie-express/routes/api.js
@@ -1,5 +1,11 @@
 import express from "express";
+import * as movieController from "../controllers/movieController.js";
 
 const api = express.Router();
+
+api.get("/movie", movieController.listMovie);
+api.post("/movie", movieController.addListMovie);
+api.put("/movie/:id", movieController.updateDataMovie);
+api.delete("/movie/:id", movieController.deleteMovie);
 
 export default api;
