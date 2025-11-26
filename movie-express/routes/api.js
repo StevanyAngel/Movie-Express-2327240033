2b234bbca120b6af40626d9885469c8b2db802a3
StevanyAngel <stevanyangel_2327240033@mhs.mdp.ac.id>
2025-11-26 17:17:57 +0700

    update api.js

--- movie-express/routes/api.js
+++ movie-express/routes/api.js
@@ -1,11 +1,22 @@
 import express from "express";
 import * as movieController from "../controllers/movieController.js";
+import * as userController from "../controllers/userController.js";
+import { authenticateTokenMiddleware } from "../../middlewares/authenticateTokenMiddleware.js";
 
 const api = express.Router();
 
+api.post("/signin", userController.signIn);
+api.post("/signup", userController.signUp);
+
+// Protected Routes (Movies)
 api.get("/movie", movieController.listMovie);
 api.post("/movie", movieController.addListMovie);
 api.put("/movie/:id", movieController.updateDataMovie);
 api.delete("/movie/:id", movieController.deleteMovie);
+
+api.get("/movie", authenticateTokenMiddleware, movieController.listMovie);
+api.post("/movie", authenticateTokenMiddleware, movieController.addListMovie);
+api.put("/movie/:id", authenticateTokenMiddleware, movieController.updateDataMovie);
+api.get("/movie/:id", authenticateTokenMiddleware, movieController.deleteMovie);
 
 export default api;
